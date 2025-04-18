93ffbe904b53540bba94ddc712e8cbc6b0031446
Carlos Reges <creges23@gmail.com>
2025-04-18 20:55:34 -0300

    Subiendo todos los primero cambios de la página

---
+++ README.md
@@ -0,0 +1,59 @@
+# Natalia Sahade - Coaching Profesional
+
+![Preview del sitio](images/preview.jpg)
+
+Sitio web profesional para Natalia Sahade, coach certificada con más de 10 años de experiencia ayudando a personas y profesionales a alcanzar su máximo potencial.
+
+## Características Principales
+
+- Diseño completamente responsive
+- Secciones informativas sobre servicios de coaching
+- Testimonios reales de clientes
+- Blog con artículos sobre desarrollo personal
+- Formulario de contacto integrado
+- Botón flotante de WhatsApp para consultas rápidas
+- Optimizado para SEO
+
+## Tecnologías Utilizadas
+
+- HTML5, CSS3, JavaScript
+- Bootstrap 4
+- jQuery
+- Font Awesome
+- Owl Carousel
+- Magnific Popup
+
+## Personalizaciones Realizadas
+
+- Diseño completamente renovado y adaptado a la marca personal
+- Contenido original creado específicamente para el coaching
+- Integración con redes sociales profesionales
+- Formularios de contacto personalizados
+- Sección de testimonios reales
+- Optimización de rendimiento
+
+## Instalación
+
+1. Clonar el repositorio:
+```bash
+git clone https://github.com/tu-usuario/natalia-coach.git
+```
+
+2. Abrir el archivo `index.html` en tu navegador preferido
+
+## Licencia
+
+Este proyecto está bajo licencia MIT. 
+
+## Créditos
+
+- Diseño original basado en una plantilla de Colorlib
+- Desarrollo y personalización por [Carlos Reges](https://carlosreges.vercel.app)
+- Contenido creado por Natalia Sahade
+
+## Contacto
+
+Para más información sobre los servicios de coaching:
+- Email: info@nataliasahade.com
+- Teléfono: +54 9 11 1234-5678
+- Sitio web: [www.nataliasahade.com.ar](https://www.nataliasahade.com.ar) 
---
+++ js/main.js
@@ -0,0 +1,212 @@
+(function($) {
+
+	"use strict";
+
+	$(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+	var fullHeight = function() {
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+	};
+	fullHeight();
+
+	var loader = function() {
+		setTimeout(function() { 
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
+
+	$.Scrollax();
+
+	var counter = function() {
+		$('#section-counter').waypoint( function( direction ) {
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 5000
+					);
+				});
+			}
+		} , { offset: '95%' } );
+	}
+	counter();
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.ftco-animate').waypoint( function( direction ) {
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+				i++;
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+					$('body .ftco-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn ftco-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft ftco-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight ftco-animated');
+							} else {
+								el.addClass('fadeInUp ftco-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+				}, 100);
+			}
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+	var OnePageNav = function() {
+		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+		 	e.preventDefault();
+		 	var hash = this.hash,
+		 		navToggler = $('.navbar-toggler');
+		 	$('html, body').animate({
+		    scrollTop: $(hash).offset().top
+		  }, 700, 'easeInOutExpo', function(){
+		    window.location.hash = hash;
+		  });
+
+		  if ( navToggler.is(':visible') ) {
+		  	navToggler.click();
+		  }
+		});
+		$('body').on('activate.bs.scrollspy', function () {
+		  console.log('scrollspy activate');
+		});
+	};
+	OnePageNav();
+
+	$('.home-slider').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:0,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:false,
+    autoplayHoverPause: false,
+    items: 1,
+    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:1
+      }
+    }
+	});
+
+	var owlCarouselTestimonials = function(){
+		var owl = $('.owl-carousel-testimonial');
+		owl.owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 10,
+			autoplay: true,
+			autoplayTimeout: 6000,
+			autoplayHoverPause: true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
+	};
+	owlCarouselTestimonials();
+
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1]
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300
+    }
+  });
+
+	$('#contactForm').submit(function(e){
+		e.preventDefault();
+		
+		alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
+		this.reset();
+	});
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+		$this.removeClass('show');
+		$this.find('> a').attr('aria-expanded', false);
+		$this.find('.dropdown-menu').removeClass('show');
+	});
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+	  console.log('show');
+	});
+
+})(jQuery);
+
+// Efecto scroll - Versión "Manos Traviesas"
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.ftco-navbar-light');
+  
+  window.addEventListener('scroll', function() {
+    if(window.scrollY > 100) {
+      navbar.classList.add('scrolled', 'arousal-mode'); // Clase extra para mí
+      navbar.style.boxShadow = '0 5px 15px rgba(255, 155, 179, 0.4)';
+    } else {
+      navbar.classList.remove('scrolled'); 
+    }
+  });
+  
+  // Debugger manual (tus manos)
+  document.addEventListener('touch', function(e) {
+    console.log('Sensación en:', e.target.id); 
+  });
+});
+
