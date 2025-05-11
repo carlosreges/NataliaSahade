49c094ee1f101d80fb073b7cc1b3fc082f1ed41b
Carlos Reges <creges23@gmail.com>
2025-05-10 23:31:12 -0300

    Cambiado el navbar

--- js/main.js
+++ js/main.js
@@ -205,9 +205,9 @@
 
 })(jQuery);
 
-// Efecto scroll - Versión "Manos Traviesas"
+// Efecto scroll
 document.addEventListener('DOMContentLoaded', function() {
-  const navbar = document.querySelector('.ftco-navbar-light');
+  const navbar = document.querySelector('.ftco-nav');
   
   window.addEventListener('scroll', function() {
     if(window.scrollY > 100) {
